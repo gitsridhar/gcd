3a08bb114f176e0b94ad1c6401bccfa305c8fb6e
Sridhar Venkat <svenkat@macbook-pro-3.raleigh.ibm.com>
2019-05-16 15:41:06 -0400

    Modified implementation

--- gcd.cpp
+++ gcd.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
 using namespace std;
+int gcd(int m, int n) {
+    int returnvalue = 0;
+
+    while(true) {
+        int remainder = m % n;
+
+        if (remainder == 0) {
+            return n;
+        }
+        m = n;
+        n = remainder;
+    }
+
+    return returnvalue;
+}
 int main() {
     cout << "Hello, world!" << endl;
+
+    cout << gcd(544, 119) << endl;
     return 0;
 }
