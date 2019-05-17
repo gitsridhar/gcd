f2b12e89a176c8b16823e77862d51ef3149b3536
Sridhar Venkat <svenkat@macbook-pro-3.raleigh.ibm.com>
2019-05-17 08:19:30 -0400

    Added googletest implementation

--- gcd.cpp
+++ gcd.cpp
@@ -1,6 +1,16 @@
+/*
+* g++ -std=c++11 -isystem ./googletest/googletest/include/ -I ./googletest/googletest/ -pthread -c ./googletest/googletest/src/gtest-all.cc
+* ar -rv libtest.a gtest-all.o
+* g++ -std=c++11 -isystem ./googletest/googletest/include/ -pthread gcd.cpp libtest.a
+*
+*/
+
 #include <iostream>
 
+#include <gtest/gtest.h>
+
 using namespace std;
+
 int gcd(int m, int n) {
     int returnvalue = 0;
 
@@ -16,9 +26,17 @@ int gcd(int m, int n) {
 
     return returnvalue;
 }
-int main() {
+
+TEST(GCDTest, gcdverify) {
+    EXPECT_EQ(17, gcd(544, 119));
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+
     cout << "Hello, world!" << endl;
 
     cout << gcd(544, 119) << endl;
-    return 0;
+
+    return RUN_ALL_TESTS();
 }
