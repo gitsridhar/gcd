24f2d5210642a94dce1942bce9866b10567d6c66
Sridhar Venkat <svenkat@macbook-pro-3.raleigh.ibm.com>
2019-05-17 09:28:08 -0400

    Added GCD Extended

--- gcd.cpp
+++ gcd.cpp
@@ -30,8 +30,67 @@ int gcd(int m, int n) {
     return returnvalue;
 }
 
+int gcd_extend_a(int m, int n) {
+    int a, aprime, b, bprime,c, d, q, r;
+
+    aprime = b = 1;
+    a = bprime = 0;
+    c = m;
+    d = n;
+
+    while(true) {
+        q = c / d;
+        r =  c % d;
+
+        if (r == 0) {
+            return a;
+        }
+
+        c = d;
+        d = r;
+        int t = aprime;
+        aprime = a;
+        a = t - (q * a);
+        t = bprime;
+        bprime = b;
+        b = t - (q * b); 
+    }
+}
+
+int gcd_extend_b(int m, int n) {
+    int a, aprime, b, bprime,c, d, q, r;
+
+    aprime = b = 1;
+    a = bprime = 0;
+    c = m;
+    d = n;
+
+    while(true) {
+        q = c / d;
+        r =  c % d;
+
+        if (r == 0) {
+            return b;
+        }
+
+        c = d;
+        d = r;
+        int t = aprime;
+        aprime = a;
+        a = t - (q * a);
+        t = bprime;
+        bprime = b;
+        b = t - (q * b); 
+    }
+}
+
 TEST(GCDTest, gcdverify) {
     EXPECT_EQ(17, gcd(544, 119));
+}
+
+TEST(GCDTest, gcdextendverify) {
+    EXPECT_EQ(5,   gcd_extend_a(1769, 551));
+    EXPECT_EQ(-16, gcd_extend_b(1769, 551));
 }
 
 int main(int argc, char **argv) {
